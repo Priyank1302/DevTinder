d515fd2326fec877e45b18ebe4f6d4de5d3a267f
Priyank <priyankmsd007@gmail.com>
2026-05-28 13:50:32 +0530

    dynamic signup

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@ const app = express();
 
 const User = require("./models/user")
 
+app.use(express.json())//reads json object , converts it in js object and we can read that body
+app.post('/signup', async (req, res) => {
 
-app.post('/signup',async(req,res)=>{
-    const userObj = {
-        firstName: "Virat",
-        lastName:" Kohli",
-        emailId: "viratmsd007@gmail.com",
-        password:"virat@123",
-        age: 37,
-        gender:"Male"
-    }
     //creating a new instance of user model
-    const user = new User(userObj)
-    await user.save();
-    res.send("User added successfully")
+    try {
+
+        const user = new User(req.body)
+        await user.save();
+        res.send("User added successfully")
+
+    }
+    catch (e) {
+      e.status(400).send("error saving the user"+ e.message)
+    }
 })
 
-connectDb().then(()=>{
+connectDb().then(() => {
     console.log("DB connection established")
-    app.listen(3000,()=>{
+    app.listen(3000, () => {
         console.log("server listening at 3000")
     });
- }).catch((error)=>{
-   console.error("error connecting to db",error)
- })
+}).catch((error) => {
+    console.error("error connecting to db", error)
+})

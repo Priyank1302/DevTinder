97bfaf3a7dd38abdab264a6d5b7ed6d143f298cd
Priyank <priyankmsd007@gmail.com>
2026-05-29 13:07:54 +0530

    validation and encryption and login api

--- src/app.js
+++ src/app.js
@@ -4,52 +4,81 @@ const connectDb = require("./config/database")
 const app = express();
 
 const User = require("./models/user")
+const { validateSignUpData } = require('./utils/validation')
+const bcrypt = require("bcrypt");
 
 app.use(express.json())//reads json object , converts it in js object and we can read that body
 app.post('/signup', async (req, res) => {
 
     //creating a new instance of user model
     try {
+        validateSignUpData(req)
+        const { firstName, lastName, emailId, password } = req.body;
 
-        const user = new User(req.body)
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const user = new User({
+            firstName, lastName, emailId, password: passwordHash
+        })
         await user.save();
         res.send("User added successfully")
 
     }
     catch (e) {
-      res.status(400).send("error saving the user"+ e.message)
+        res.status(400).send("ERROR" + e.message)
     }
 })
 
-app.get("/user",async(req,res)=>{
-   const email = req.body.emailId
-   const user =await User.find({emailId:email})
-   res.send(user)
-})
-
-app.get("/feed",async(req,res)=>{
-   
+app.post("/login",async(req,res)=>{
     try{
-        const user =await User.find({})
-       res.send(user)
-    }
-    catch(e)
+        const {emailId,password} = req.body;
+        // find user by email
+        const user = await User.findOne({ emailId });
+        if (!user) {
+            return res.status(400).send("Your account is not registered with this email id!")
+        }
+
+        const isPasswordvalid = await bcrypt.compare(password,user.password)
+        if(!isPasswordvalid)
+        {
+            return res.status(400).send("Invalid credentials!")
+        }
+        res.send("User Logged In Successfully!")
+
+    }catch(e)
     {
+        res.status(400).send("ERROR :" + e.message)
+    }
+})
+
+app.get("/user", async (req, res) => {
+    const email = req.body.emailId
+    const user = await User.find({ emailId: email })
+    res.send(user)
+})
+
+app.get("/feed", async (req, res) => {
+
+    try {
+        const user = await User.find({})
+        res.send(user)
+    }
+    catch (e) {
         res.status(400).send("something went wrong")
     }
 })
 
- app.delete("/user", async (req, res) => {
+app.delete("/user", async (req, res) => {
     try {
         const userId = req.body.userId; // move inside try!
         const user = await User.findByIdAndDelete(userId)
         res.send("User deleted successfully!")
-    } catch(e) {
+    } catch (e) {
         res.status(400).send("something went wrong")
     }
 })
 
-app.patch("/user/:userId",async(req,res)=>{
+app.patch("/user/:userId", async (req, res) => {
     try {
 
         // {
@@ -57,12 +86,11 @@ app.patch("/user/:userId",async(req,res)=>{
         //     "age": 30,
         //     "gender": "female"
         //  } we willl loop thriugh every keys of object requested by user and will check if only every allowed updates is there
-        const updates =req.body
+        const updates = req.body
         const userId = req.params?.userId; // move inside try!
-        const ALLOWED_UPDATES = ["photoURL","about","gender","age","skills"];
-        const isUpdateAllowed = Object.keys(updates).every(k=>ALLOWED_UPDATES.includes(k));
-        if(!isUpdateAllowed)
-        {
+        const ALLOWED_UPDATES = ["photoURL", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(updates).every(k => ALLOWED_UPDATES.includes(k));
+        if (!isUpdateAllowed) {
             res.status(400).send("Update not allowed")
         }
         if (updates.skills && updates.skills.length > 5) {
@@ -71,13 +99,13 @@ app.patch("/user/:userId",async(req,res)=>{
         const user = await User.findByIdAndUpdate(
             userId,      // 1st arg → id
             updates,     // 2nd arg → what to update
-            { 
+            {
                 runValidators: true,      // runs schema validation
                 returnDocument: "after"   // returns updated doc
             }
         )
         res.send("User updated successfully!")
-    } catch(e) {
+    } catch (e) {
         res.status(400).send("something went wrong")
     }
 })
